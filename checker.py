0b5b987d029344fbf3a5860963b8f538993f8fa6
Emanuele <eminik2006@gmail.com>
2021-07-15 15:50:07 +0200

    Funzionante

--- checker.py
+++ checker.py
@@ -5,5 +5,7 @@ def check(*args):  # has to get arguments to run with `bind()`
     test.get_best_server()
     down = test.download()
     up   = test.upload()
+    # print('Download Speed: {:5.2f} Mb'.format( down/(1024*1024) ))
+    # print('Upload Speed: {:5.2f} Mb'.format(   up/(1024*1024) ))
     
     return down, up
--- main.py
+++ main.py
@@ -1,187 +1,196 @@
+import checker
+import kivy
+from kivymd.uix.screen import MDScreen
+from kivymd.app import MDApp
+from kivy.uix.image import Image
+from kivymd.uix.button import MDFillRoundFlatIconButton, MDFillRoundFlatButton
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.label import MDLabel
+from kivymd.uix.toolbar import MDToolbar
+from kivy.uix.widget import Widget
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.properties import ObjectProperty
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.theming import ThemeManager
-
-class MyLayout(BoxLayout):
-
-    scr_mngr = ObjectProperty(None)
-
-    def check_data_login(self):
-        username = self.scr_mngr.screen1.username.text
-        password = self.scr_mngr.screen1.password.text
-
-        print(username)
-        print(password)
-
-        if username == "" and password == "":
-            self.change_screen("screen2")
+import time
+import speedtest
 
 
-    def change_screen(self, screen, *args):
-        self.scr_mngr.current = screen
-        
-        
+class SpeedtestApp(MDApp):
     def flip(self):
-        print("Button pressed")
-
-# Costruzione del layout dell'app
-KV = """
-#:import Toolbar kivymd.toolbar.Toolbar
-#:import ThemeManager kivymd.theming.ThemeManager
-#:import MDNavigationDrawer kivymd.navigationdrawer.MDNavigationDrawer
-#:import NavigationLayout kivymd.navigationdrawer.NavigationLayout
-#:import NavigationDrawerDivider kivymd.navigationdrawer.NavigationDrawerDivider
-#:import NavigationDrawerToolbar kivymd.navigationdrawer.NavigationDrawerToolbar
-#:import NavigationDrawerSubheader kivymd.navigationdrawer.NavigationDrawerSubheader
-#:import MDCheckbox kivymd.selectioncontrols.MDCheckbox
-#:import MDSwitch kivymd.selectioncontrols.MDSwitch
-#:import MDList kivymd.list.MDList
-#:import OneLineListItem kivymd.list.OneLineListItem
-#:import TwoLineListItem kivymd.list.TwoLineListItem
-#:import ThreeLineListItem kivymd.list.ThreeLineListItem
-#:import OneLineAvatarListItem kivymd.list.OneLineAvatarListItem
-#:import OneLineIconListItem kivymd.list.OneLineIconListItem
-#:import OneLineAvatarIconListItem kivymd.list.OneLineAvatarIconListItem
-#:import MDTextField kivymd.textfields.MDTextField
-#:import MDSpinner kivymd.spinner.MDSpinner
-#:import MDCard kivymd.card.MDCard
-#:import MDSeparator kivymd.card.MDSeparator
-#:import MDDropdownMenu kivymd.menu.MDDropdownMenu
-#:import get_color_from_hex kivy.utils.get_color_from_hex
-#:import colors kivymd.color_definitions.colors
-#:import SmartTile kivymd.grid.SmartTile
-#:import MDSlider kivymd.slider.MDSlider
-#:import MDTabbedPanel kivymd.tabs.MDTabbedPanel
-#:import MDTab kivymd.tabs.MDTab
-#:import MDProgressBar kivymd.progressbar.MDProgressBar
-#:import MDAccordion kivymd.accordion.MDAccordion
-#:import MDAccordionItem kivymd.accordion.MDAccordionItem
-#:import MDAccordionSubItem kivymd.accordion.MDAccordionSubItem
-#:import MDThemePicker kivymd.theme_picker.MDThemePicker
-#:import MDBottomNavigation kivymd.tabs.MDBottomNavigation
-#:import MDBottomNavigationItem kivymd.tabs.MDBottomNavigationItem
-
-#:import partial functools.partial
-
-MyLayout:
-    scr_mngr: scr_mngr
-    orientation: 'vertical'
-
-    ScreenManager:
-        id: scr_mngr
-        screen1: screen1
-
-        Screen:
-            id: screen1
-            name: 'screen1'
-            username: username
-            password: password
-
-            MDCard:
-                size_hint: None, None
-                size: dp(520), dp(340)
-                pos_hint: {'center_x': 0.5, 'center_y': 0.5}
-
-                BoxLayout:
-                    orientation:'vertical'
-                    padding: dp(20)
-                    spacing:20
-
-                    MDLabel:
-                        text: 'Connection'
-                        theme_text_color: 'Secondary'
-                        font_style:"Title"
-                        size_hint_y: None
-                        height: dp(36)
-
-                    MDSeparator:
-                        height: dp(1)
-
-                    MDTextField:
-                        id: username
-                        hint_text: "Username "
-                        helper_text_mode: "on_focus"
-
-                    MDTextField:
-                        id: password
-                        hint_text: "Password "
-                        helper_text_mode: "on_focus"
-                        password: True
-
-                    MDFlatButton:
-                        text: "Connection"
-                        pos_hint: {'center_x': 0.5}
-                        on_release: root.check_data_login()
-        Screen:
-            name: 'screen2'
-
-            Toolbar:
-                id: toolbar
-                title: "Welcome ! "
-                pos_hint: {'top':1}
-                md_bg_color: app.theme_cls.primary_color
-                background_palette: 'DeepPurple'
-                background_hue: 'A400'
-                left_action_items: [['arrow-left', partial(root.change_screen, 'screen1') ]]
-                right_action_items: [['animation', lambda x: root.flip()]]
-
-            Image:
-                source: 'logo.png'
-                size: self.texture_size
-                pos_hint: {"center_x": 0.5, "center_y": 0.69 }
-                size_hint: (0.3,0.3)
-            
-            MDLabel:
-                font_style: 'Title'
-                text: "Download Speed"
-                theme_text_color: "Custom"
-                text_color: 1.2, 0.6, 0.5, 1
-                height: self.texture_size[1] + dp(3)
-                halign: 'center'
-                pos_hint: {'center_x': 0.5, 'center_y': 0.45}
-                
-            MDLabel:
-                font_style: 'Title'
-                text: "-"
-                theme_text_color: "Custom"
-                text_color: 1.2, 0.6, 0.5, 1
-                height: self.texture_size[1] + dp(3)
-                halign: 'center'
-                pos_hint: {'center_x': 0.5, 'center_y': 0.35}
-                
-            MDLabel:
-                font_style: 'Title'
-                text: "Upload Speed"
-                theme_text_color: "Custom"
-                text_color: 1, 0, 0.2, 1
-                height: self.texture_size[1] + dp(3)
-                halign: 'center'
-                pos_hint: {'center_x': 0.5, 'center_y': 0.25}
-                
-            MDLabel:
-                font_style: 'Title'
-                text: "-"
-                theme_text_color: "Custom"
-                text_color: 1, 0, 0.2, 1
-                height: self.texture_size[1] + dp(3)
-                halign: 'center'
-                pos_hint: {'center_x': 0.5, 'center_y': 0.15}
-"""
-
-#Available vanilla font_style are: ‘'Body1', 'Body2', 'Caption', 'Subhead', 'Title', 'Headline', 'Display1', 'Display2', 'Display3', 'Display4', 'Button', 'Icon'.
-
-
-class SpeedtestApp(App):
-    title = "Speedtest App"
-    theme_cls = ThemeManager()
-    theme_cls.primary_palette = 'Teal'
-    theme_cls.accent_palette = 'Blue'
-    # theme_cls.theme_style = 'Dark'
+        """[summary]
+        """
+        if self.state == 0:
+            self.state = 1
+            self.toolbar.title = "Decimal to Binary"
+            self.input.text = "Enter a Decimal number"
+            self.converted.text = "" 
+            self.label.text = "" 
+        else:
+            self.state = 0
+            self.toolbar.title = "Binary to Decimal"
+            self.input.text = "Enter a Binary number"
+            self.converted.text = "" 
+            self.label.text = ""   
+        
     
+    def convert(self, args):
+        """[summary]
+        Args:
+            args ([type]): [description]
+        """
+        
+        # Conversione bin -> dec
+        if self.state == 0:   
+            # Prelevo il numero immesso nella finestra 
+            usr_input = self.input.text
+            
+            # Check se non è stato inserito un valore valido
+            if usr_input == '' or usr_input == 'Enter a Binary Number':
+                self.label.text = "Enter a valid number "
+                self.converted.text = ""
+            else:
+                # Converto in binario
+                val = int(usr_input, 2)
+                
+                # Aggiorna il testo di "converted" con il risultato della conversione 
+                self.converted.text = str(val)
+                
+                self.label.text = "In decimal is: "
+        
+        # Conversione dec -> bin
+        else:
+            # Prelevo il numero immesso nella finestra 
+            usr_input = self.input.text
+            
+            # Check se non è stato inserito un valore valido
+            if usr_input == '' or usr_input == 'Enter a Decimal number' :
+                self.label.text = "Enter a valid number "
+                self.converted.text = ""
+            else:
+                # Converto in Decimale: le stringhe una volta convertite in
+                # binario sono nella forma 0b1000 con [2:] elimino 0b
+                val = bin(int(usr_input))[2:]
+                
+                # Aggiorna il testo di "converted" con il risultato della conversione 
+                self.converted.text = str(val)
+                
+                self.label.text = "In binary is: "
+    
+    
+    def test_connection(self, *args):  # has to get arguments to run with `bind()`
+        
+        self.downspeed_label_value.text = "Testing Connection"
+        self.upspeed_label_value.text = "Testing Connection"
+        
+        down, up = checker.check(self)
+           
+        down = round(down/(1024*1024),3)
+        up = round(up/(1024*1024),3)
+        
+        self.downspeed_label_value.text = str(down) + " Mbps"
+        self.upspeed_label_value.text = str(up) + " Mbps"
+    
+        
     def build(self):
-        return Builder.load_string(KV)
+        """[summary]
+        Returns:
+            [type]: [description]
+        """
+        # Stato iniziale 
+        self.state = 0
+        
+        self.theme_cls.primary_palette = "Teal"
+        
+        # Display della schermata
+        screen = MDScreen()
+        
+        # Dichiaro l'oggetto toolbar
+        self.toolbar = MDToolbar(
+            title="Speedtest",
+            pos_hint = {"top":1}
+            )
+        
+        # Aggiunge una icona nella toolbar  e premendola si attiva la funzione flip()
+        # self.toolbar.right_action_items = [
+        # ["rotate-3d-variant", lambda x:self.flip()]]
+        
+        # Aggiunge la Toolbar alla finestra
+        screen.add_widget(self.toolbar)
+        
+        # Logo
+        self.img = Image(
+            source ='logo.png',
+            allow_stretch = False,
+            keep_ratio = True,
+            pos_hint =  {"center_x": 0.5, "center_y": 0.74 },
+            size_hint = (0.3,0.3),             
+            )
+             
+        screen.add_widget(self.img)
+        
+        # # Widget per collezionare l'input dell'utente
+        # self.input = MDTextField(
+        #     text = "Enter a Binary Number",
+        #     halign = "center",
+        #     size_hint = (0.8,1),
+        #     pos_hint = {"center_x": 0.5, "center_y": 0.45 },
+        #     font_size = 22
+        # )
+        
+        # screen.add_widget(self.input)
+        
+        # Pulsante Test Connessione
+        screen.add_widget(MDFillRoundFlatButton(
+            text= "Test Connection",
+            font_size = 17,
+            pos_hint = {"center_x": 0.5, "center_y": 0.50 },
+            on_press = self.test_connection
+            ))
+        
+        # Label Testo Download
+        self.downspeed_label = MDLabel(
+            text = "Download Speed",
+            halign = "center",
+            pos_hint = {"center_x": 0.5, "center_y": 0.35 },
+            theme_text_color = "Custom" ,
+            text_color= [0.4, 0.75, 0.3, 1]
+        )
+        
+        # Label valore Download
+        self.downspeed_label_value = MDLabel(
+            text = "--",
+            halign = "center",
+            pos_hint = {"center_x": 0.5, "center_y": 0.275 },
+            theme_text_color = "Custom" ,
+            text_color= [0.4, 0.75, 0.3, 1]
+        )
+        
+        # Label Testo Upload
+        self.upspeed_label = MDLabel(
+            text = "Upload Speed",
+            halign = "center",
+            pos_hint = {"center_x": 0.5, "center_y": 0.20 },
+            theme_text_color = "Custom",
+            text_color= [0.8, 0.2, 0, 1] 
+        )
+        
+        # Label valore Upload
+        self.upspeed_label_value = MDLabel(
+            text = "--",
+            halign = "center",
+            pos_hint = {"center_x": 0.5, "center_y": 0.125 },
+            theme_text_color = "Custom",
+            text_color= [0.8, 0.2, 0, 1] 
+        )
+        
+        # Aggiungo le label alla schermata
+        screen.add_widget(self.downspeed_label)
+        screen.add_widget(self.downspeed_label_value)
+        screen.add_widget(self.upspeed_label)
+        screen.add_widget(self.upspeed_label_value)
+        
+        
+        return screen
 
-
-SpeedtestApp().run()
+if __name__ == '__main__':
+    SpeedtestApp().run()
